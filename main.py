8763a8bf690f96cf1b715e785229a2915df1aa23
MinAhPark57 <minap253@gmail.com>
2024-04-13 01:09:41 +0900

    글쓰기페이지 서버연결

--- main.py
+++ main.py
@@ -2,16 +2,33 @@ from fastapi import FastAPI, UploadFile, Form
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 
+#SQL Lite 사용할 준비
+import sqlite3
+con = sqlite3.connect('db.db',check_same_thread=False) 
+cur = con.cursor()
+
+
+
 app = FastAPI()
 
 
 @app.post('/items')
-def create_item(image:UploadFile, 
+async def create_item(image:UploadFile, 
                 title:Annotated[str,Form()], #title정보는 Form데이터 형식으로 문자열(str)
                 price:Annotated[int,Form()], 
-                desc:Annotated[str,Form()], 
+                description:Annotated[str,Form()], 
                 place:Annotated[str,Form()]):
-    print(image,title,price,desc,place)
+    
+    #이미지 읽을 시간 확보
+    image_bytes = await image.read()
+    
+    cur.execute(f""" 
+                INSERT INTO items (title,image,price,description,place)
+                VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}')                 
+                """) #f문자열, items라는 테이블에 들어갈 값 설정. image_bytes 16진법으로 저장: hex()
+    
+    con.commit()    
+    
     return '200' #ok 사인
 
 

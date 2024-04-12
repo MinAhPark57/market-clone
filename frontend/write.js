220f4c421cbec8b8c0d0a24bd15713f9f2848843
MinAhPark57 <minap253@gmail.com>
2024-04-13 01:29:54 +0900

    글쓰기완료하면 메인 화면으로 이동 추가
    완료 버튼을 누르면
    root 경로로 이동하는 기능 추가

--- frontend/write.js
+++ frontend/write.js
@@ -2,11 +2,23 @@ const form = document.getElementById("write-form");
 
 const handleSubmitForm = async (event) => {
   event.preventDefault(); //submit 했을 때 자동 새로고침 막기
-  await fetch("/items", {
-    method: "POST",
-    body: new FormData(form),
-  });
-  console.log("제출완료");
+
+  try {
+    const res = await fetch("/items", {
+      method: "POST",
+      body: new FormData(form),
+    });
+
+    const data = await res.json(); // 응답을 데이터로 바꾸기
+
+    //서버에서 내려주기로 한 200이면 메인페이지로 이동 실행
+    if (data === "200") {
+      window.location.pathname = "/";
+      console.log("완료");
+    }
+  } catch (e) {
+    console.error(e); //에러 출력
+  }
 };
 
 form.addEventListener("submit", handleSubmitForm);
